3de630cdf2645872fb9daa14e90a2c3c3101be79
alejo9424 <alejandro.casta3447@gmail.com>
2023-03-06 16:25:35 -0500

    Initial

---
+++ ecs/form-app/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+require('dotenv').config();
+const bodyParser = require("body-parser");
+const AWS = require('aws-sdk');
+const app = express();
+const s3 = new AWS.S3();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/health", (req, res) => {
+    res.status(200).send('Service runing ok.')
+});
+
+app.post('/student', (req, res) => {
+    const data = JSON.stringify(req.body);
+    const params = {
+        Bucket: process.env.BUCKET,
+        Key: `${data.name}.json`,
+        Body: data
+    };
+    s3.upload(params, (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send(`Error saving data to S3: ${err}`);
+        } else {
+            console.log(`Data saved to S3: ${data.Location}`);
+            res.send(`Data saved to S3 review the AWS console: ${data}`);
+        }
+    });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on 3000");
+});
